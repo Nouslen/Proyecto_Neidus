64b8670241cb15a03cc8c882429cad137eab24fe
Nelson Galleguillos <nd_galleguillos@hotmail.com>
2020-07-03 18:05:03 -0400

    Update inicio_al.php

--- inicio_al.php
+++ inicio_al.php
@@ -3,12 +3,13 @@ include("function/conexion.php");
 session_start();
 
 
-
+//algo literal
 
  ?>
 
 
  alumno aeaaaaaa
+
  
  <br><br><br><br><br><br>
 <center><input type="image" src="https://media1.tenor.com/images/7a27a55c5cf73af8ffc0968aed28a6a8/tenor.gif?itemid=12246997" ></center>
